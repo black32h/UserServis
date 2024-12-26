1d18330559cb5902c59eebd54b35262d4ffb3fa3
black32h <blackspetya@gmail.com>
2024-12-26 11:51:04 +0200

    Update UserServiceApplication.java

--- src/main/java/com/example/userservis/UserServiceApplication.java
+++ src/main/java/com/example/userservis/UserServiceApplication.java
@@ -1,25 +1,13 @@
 package com.example.userservis;
 
-import org.apache.catalina.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @SpringBootApplication
-public class UserServisApplication {
-
+public class UserServiceApplication{
     public static void main(String[] args) {
-        SpringApplication.run(UserServisApplication.class, args);
+        SpringApplication.run(UserServiceApplication.class, args);
     }
-
 }
-@RestController
-@RequestMapping("/api/users")
-class UserController {
-    private final Map<Long, User> users = new HashMap<>();
 
-}
